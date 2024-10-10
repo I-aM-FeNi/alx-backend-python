16cad9b5ba37f775d8cc3753f41f3e3f61865355
malcolms-anatomy <malcolmihere@outlook.com>
2024-10-10 16:36:18 +0000

    Updated all files

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -11,7 +11,7 @@ def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     Sum a list of integers and floats.
 
     Args:
-        mxd_lst (List[Union[int, float]]): The list of integers and float numbers.
+        mxd_lst (List[Union[int, float]]): List of integers and float numbers.
 
     Returns:
         float: The sum of the numbers.
