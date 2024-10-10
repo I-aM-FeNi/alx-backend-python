dd07a6c56f9f5c5328f60eaf1db736bf5dc8fa8b
malcolms-anatomy <malcolmihere@outlook.com>
2024-10-10 17:17:40 +0000

    Updated all files

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to safely get a
+value from a dictionary.
+"""
+
+from typing import Mapping, Any, Union, TypeVar
+
+T = TypeVar('T')
+
+
+def safely_get_value(
+    dct: Mapping[Any, Any], key: Any, default: Union[T, None] = None
+) -> Union[Any, T]:
+    """
+    Safely get a value from a dictionary.
+
+    Args:
+        dct (Mapping[Any, Any]): The dictionary.
+        key (Any): The key to look for.
+        default (Union[T, None]): Default value if the key is not found.
+
+    Returns:
+        Union[Any, T]: The value associated with the key if it exists,
+        otherwise the default value.
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
